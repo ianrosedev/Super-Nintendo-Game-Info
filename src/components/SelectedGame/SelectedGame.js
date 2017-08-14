fa3bda46546379153327ac389705465857d0c82f
Ian Rose <ianrosedev@gmail.com>
2017-08-14 12:28:36 -0400

    Worked on SelectedGame - ready to add VideosList

--- src/components/SelectedGame/SelectedGame.js
+++ src/components/SelectedGame/SelectedGame.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GameDetail from '../../containers/GameDetail/GameDetail';
 import BasicContainer from '../BasicContainer/BasicContainer';
+import HeaderContainer from '../HeaderContainer/HeaderContainer';
 import './SelectedGame.css';
 
 const SelectedGame = () => (
@@ -8,6 +9,13 @@ const SelectedGame = () => (
     <BasicContainer>
       <GameDetail />
     </BasicContainer>
+    <HeaderContainer
+      title='Videos'
+      icon='fa fa-video-camera'
+    >
+      <p>Testing</p>
+      <p>123</p>
+    </HeaderContainer>
   </div>
 );
 
