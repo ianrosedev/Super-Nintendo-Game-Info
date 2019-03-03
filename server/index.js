f4d5a95f6e04dbde276219786bc3f86173bc8e0e
Ian Rose <ianrosedev@gmail.com>
2019-03-03 16:30:37 -0500

    Redirect HTTP to HTTPS for all routes

--- server/index.js
+++ server/index.js
@@ -6,6 +6,15 @@ import youtubeSearch from 'youtube-search';
 // App
 const app = express();
 
+// Redirect HTTP to HTTPS for all routes
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    else next();
+  });
+}
+
 // Security
 app.use(helmet());
 
